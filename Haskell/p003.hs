6f72ffd5a55ccbe45b01e27db95e38f2a8f7a09a
Kent Frazier <kentfrazier@gmail.com>
2010-11-06 16:30:45 -0500

    Made largestPrimeFactor smart enough to respond with Nothing when there are no prime factors.  Reordered code some to improve readability.

--- Haskell/p003.hs
+++ Haskell/p003.hs
@@ -3,19 +3,26 @@
 --What is the largest prime factor of the number 600851475143 ?
 
 primeFactors :: (Integral a) => a -> [a]
+primeFactors 0 = []
 primeFactors 1 = []
-primeFactors 2 = [2]
-primeFactors 3 = [3]
-primeFactors num = if numIsPrime then [num] else factor:rest
-    where limit = floor . sqrt $ fromIntegral num
-          notMultiple = (/=0) . (mod num)
-          options = dropWhile notMultiple $ 2:[3,5..limit]
+primeFactors num =
+    if numIsPrime
+        then [num]
+        else factor:rest
+    where options = dropWhile notMultiple $ 2:[3,5..limit]
+              where notMultiple = (/=0) . (mod num)
+                    limit = floor . sqrt $ fromIntegral num
           numIsPrime = options == []
           factor = head options
           rest = primeFactors $ div num factor
 
-largestPrimeFactor :: (Integral a, Ord a) => a -> a
-largestPrimeFactor num = foldl max 0 $ primeFactors num
+largestPrimeFactor :: (Integral a, Ord a) => a -> Maybe a
+largestPrimeFactor num =
+    if factors == []
+        then Nothing
+        else Just largest
+    where factors = primeFactors num
+          largest = foldl max 0 factors
 
 main :: IO ()
 main = print $ largestPrimeFactor 600851475143
