3f275470cd25367943975a530c3627e2dbe5c72b
Kent Frazier <kentfrazier@gmail.com>
2010-11-06 15:58:46 -0500

    Solved problem 3 in Haskell

---
+++ Haskell/p003.hs
@@ -0,0 +1,22 @@
+--The prime factors of 13195 are 5, 7, 13 and 29.
+--
+--What is the largest prime factor of the number 600851475143 ?
+
+primeFactors :: (Integral a) => a -> [a]
+primeFactors 1 = []
+primeFactors 2 = [2]
+primeFactors 3 = [3]
+primeFactors num = if numIsPrime then [num] else some ++ rest
+    where limit = floor . sqrt $ fromIntegral num
+          notMultiple = (/=0) . (mod num)
+          options = dropWhile notMultiple $ 2:[3,5..limit]
+          numIsPrime = options == []
+          factor = head options
+          some = primeFactors factor
+          rest = primeFactors $ div num factor
+
+largestPrimeFactor :: (Integral a, Ord a) => a -> a
+largestPrimeFactor num = foldl max 0 $ primeFactors num
+
+main :: IO ()
+main = print $ largestPrimeFactor 600851475143
