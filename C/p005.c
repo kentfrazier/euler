a36e0d3ad252ac2246ae49b8ca77aa78695cd7a2
Kent Frazier <kentfrazier@gmail.com>
2010-11-14 19:48:38 -0600

    Solved problem 7 in C

--- C/p005.c
+++ C/p005.c
@@ -10,72 +10,9 @@ numbers from 1 to 20?
 #include <math.h>
 #include <stdlib.h>
 
-typedef struct {
-    int *numbers;
-    int position;
-    int limit;
-} PrimeSieve;
+#include <primes.h>
 
-PrimeSieve make_prime_sieve(int);
-void reset_prime_sieve(PrimeSieve *);
-void cleanup_prime_sieve(PrimeSieve *);
-int next_prime(PrimeSieve *, int *);
-int is_prime(int, PrimeSieve *);
 int smallest_divisible_by_range(int);
-
-PrimeSieve make_prime_sieve(int limit) {
-    int *numbers = malloc(sizeof(int) * (limit + 1));
-
-    PrimeSieve sieve = { numbers, 1, limit };
-
-    int i;
-    for (i = 2; i <= limit; i++) {
-        *(sieve.numbers + i) = 1;
-    }
-
-    double limit_sqrt = sqrt((double) limit);
-
-    int num,
-        multiple = 0;
-    for (num = 2; num <= limit_sqrt; num++) {
-        for (i = 2; ; i++) {
-            multiple = i * num;
-            if (multiple > limit) {
-                break;
-            }
-            *(sieve.numbers + multiple) = 0;
-        }
-    }
-
-    return sieve;
-}
-
-void cleanup_prime_sieve(PrimeSieve *sieve) {
-    free(sieve->numbers);
-}
-
-void reset_prime_sieve(PrimeSieve *sieve) {
-    sieve->position = 1;
-}
-
-int is_prime(int num, PrimeSieve *sieve) {
-    return *(sieve->numbers + sieve->position);
-}
-
-int next_prime(PrimeSieve *sieve, int *prime) {
-    do {
-        sieve->position += 1;
-    } while ((sieve->position <= sieve->limit) &&
-            !*(sieve->numbers + sieve->position));
-
-    if (sieve->position > sieve->limit) {
-        printf("Exceeded limit: position: %d, limit: %d\n",
-               sieve->position, sieve->limit);
-        return 1;
-    }
-    *prime = sieve->position;
-    return 0;
-}
 
 int smallest_divisible_by_range(int limit) {
     int *prime_counts = malloc(sizeof(int) * (limit + 1)),
@@ -136,4 +73,5 @@ int smallest_divisible_by_range(int limit) {
 
 int main() {
     printf("%d\n", smallest_divisible_by_range(20));
+    return 0;
 }
