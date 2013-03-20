662e0be3ad9028e25bea9d88b5796427b44c6383
Kent Frazier <kentfrazier@gmail.com>
2013-03-20 00:11:13 -0500

    Reimplement p002 with goroutines and channels

--- go/p002.go
+++ go/p002.go
@@ -4,20 +4,24 @@ import "fmt"
 
 // fibonacci returns a function that can be called to return the next number
 // in the fibonacci sequence
-func fibonacci() func() int {
+func fibonacci(c chan int) {
 	current := 0
 	next := 1
-	return func() int {
-		result := current
+	for {
+		c <- current
 		current, next = next, current+next
-		return result
 	}
 }
 
 func evenFibSum(limit int) int {
-	fib := fibonacci()
+	c := make(chan int)
 	sum := 0
-	for n := fib(); n <= limit; n = fib() {
+
+	go fibonacci(c)
+	for n := range c {
+        if n > limit {
+            break;
+        }
 		if n%2 == 0 {
 			sum += n
 		}
